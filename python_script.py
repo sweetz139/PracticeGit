84fd73542ebf3c9ff5928a77ce07b85c8f4a1c0b
Zachary Sweet <zachary.sweet200@gmail.com>
2020-06-20 19:24:53 -0500

    Created python_script added feature to print today's Date

---
+++ python_script.py
@@ -0,0 +1,13 @@
+#This is a script that runs in python
+######################################################################
+#                                                                    #
+#                                                                    #
+#                        Today is a day                              #
+######################################################################
+
+from datetime import date
+
+"""Printing today's date"""
+today = date.today()
+print("Today's date:, today")
+
