a778d28d9bf212eabfd29e7f784d3767fa6c3d17
Zachary Sweet <ztsc97@mst.edu>
2020-08-04 21:20:14 -0500

    Changed everything

--- python_script.py
+++ python_script.py
@@ -1,13 +1,25 @@
-#This is a script that runs in python
-######################################################################
-#                                                                    #
-#                                                                    #
-#                        Today is a day                              #
-######################################################################
+#Creating a cool conditional statement
 
-from datetime import date
+name = input("Enter your name: ")
 
-"""Printing today's date"""
-today = date.today()
-print("Today's date:, today")
+ascii_name = ''
+MAXIMUM_LENGTH = 6
+
+
+if (len(name) > MAXIMUM_LENGTH):
+    print("Wow why such a long name?")
+
+else:
+    print("That is the right amount of letters for any name!")
+
+for char in name:
+    ascii_name += str(ord(char))
+
+
+print("Well either way whether your name is long or short" +
+        "here is your name in ascii characters {}".format(ascii_name))
+
+
+
+
 
